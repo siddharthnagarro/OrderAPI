ab0ce9afbb9e3db3ac775cfbd153efe40694e79c
Siddharth Jain <siddharth.jain01@nagarro.com>
2019-09-05 22:54:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
 
-docker-compose down && docker-compose up --build -d
+#if alredy running to stop containers
+docker-compose down
+
+#building the container
+docker-compose up --build -d
 
 echo "===== Installing Project Dependencies ====="
-docker-compose run composer install  --ignore-platform-reqs --no-interaction --no-progress --quiet
+docker-compose run composer install --prefer-source --ignore-platform-reqs --no-interaction --no-progress --quiet
 
-docker exec orderapi_php bash -c 'chmod 777 -R /var/www/html'
-
-echo "===== Clearing cache and running migrations ====="
-docker exec orderapi_php php artisan config:cache
-docker exec orderapi_php php artisan cache:clear
-docker exec orderapi_php php artisan optimize:clear
+docker exec orderapi_php bash -c 'chmod 777 -R /var/www/html && php artisan config:cache && php artisan cache:clear && php artisan optimize:clear'
 
 docker exec orderapi_php php artisan migrate
 
-echo "===== Running Unit test cases ====="
-docker exec orderapi_php php ./vendor/phpunit/phpunit/phpunit ./tests/Unit
-
-echo "===== Running Feature test cases ====="
-docker exec orderapi_php php ./vendor/phpunit/phpunit/phpunit ./tests/Feature
-
-exit 0
+docker exec orderapi_php php ./vendor/phpunit/phpunit/phpunit
