b9bd59a8a18137e911e1566cf3df6b365083716c
Siddharth Jain <siddharth.jain01@nagarro.com>
2019-09-05 17:54:30 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+docker-compose down && docker-compose up --build -d
+
+echo "===== Installing Project Dependencies ====="
+docker-compose run composer install  --ignore-platform-reqs --no-interaction --no-progress --quiet
+
+docker exec orderapi_php bash -c 'chmod 777 -R /var/www/html'
+
+echo "===== Clearing cache and running migrations ====="
+docker exec orderapi_php php artisan config:cache
+docker exec orderapi_php php artisan cache:clear
+docker exec orderapi_php php artisan optimize:clear
+
+docker exec orderapi_php php artisan migrate
+
+echo "===== Running Unit test cases ====="
+docker exec orderapi_php php ./vendor/phpunit/phpunit/phpunit ./tests/Unit
+
+echo "===== Running Feature test cases ====="
+docker exec orderapi_php php ./vendor/phpunit/phpunit/phpunit ./tests/Feature
+
+exit 0
