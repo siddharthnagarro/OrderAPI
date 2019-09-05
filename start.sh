7d0460236fa0562c84cda6aa4292b335c614c471
Siddharth Jain <siddharth.jain01@nagarro.com>
2019-09-05 17:27:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/env bash
 
-echo "***** Initializing Docker Files ***** "
 docker-compose down && docker-compose up --build -d
 
-echo "***** Installing Project Dependencies ***** "
-docker-compose run composer install
+echo "===== Installing Project Dependencies ====="
+docker-compose run composer install  --ignore-platform-reqs --no-interaction --no-progress --quiet
 
-echo "***** Adding permissions ***** "
 docker exec orderapi_php bash -c 'chmod 777 -R /var/www/html'
 
-echo "***** Clearing cache ***** "
-docker exec orderapi_php php artisan config:cache
-docker exec orderapi_php php artisan cache:clear
-docker exec orderapi_php php artisan optimize:clear
+echo "===== Clearing cache and running migrations ====="
+docker exec orderapi_php php artisan config:cache && php artisan cache:clear && php artisan optimize:clear
 
-echo "***** Running Migrations ***** "
 docker exec orderapi_php php artisan migrate
 
-echo "***** Running Unit test cases ***** "
+echo "===== Running Unit test cases ====="
 docker exec orderapi_php php ./vendor/phpunit/phpunit/phpunit ./tests/Unit
 
-echo "***** Running Feature test cases ***** "
+echo "===== Running Feature test cases ====="
 docker exec orderapi_php php ./vendor/phpunit/phpunit/phpunit ./tests/Feature
 
 exit 0
