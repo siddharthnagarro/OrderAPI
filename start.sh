fc3d4603f093591696334e715deb5979155d9a65
Siddharth Jain <siddharth.jain01@nagarro.com>
2019-09-06 10:15:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ docker-compose up --build -d
 echo "===== Installing Project Dependencies ====="
 docker-compose run composer install --prefer-source --ignore-platform-reqs --no-interaction --no-progress --quiet
 
-docker exec orderapi_php bash -c 'chmod 777 -R /var/www/html && php artisan config:cache && php artisan cache:clear && php artisan optimize:clear'
+docker exec orderapi_php bash -c 'chmod 777 -R /var/www/html && php artisan optimize:clear'
 
 docker exec orderapi_php php artisan migrate
 
